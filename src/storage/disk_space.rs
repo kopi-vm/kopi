752d2b3ad7d87541ee42aec1cd55380591c1ca36
sato taichi <sato.taichi@dentsusoken.com>
2025-06-30 03:46:26 +0000

    プラットフォーム依存性のあるコードを削除

--- src/storage/disk_space.rs
+++ src/storage/disk_space.rs
@@ -27,102 +27,26 @@ impl DiskSpaceChecker {
             target_dir
         );
 
-        #[cfg(unix)]
-        {
-            self.check_disk_space_unix(&target_dir)?;
-        }
-
-        #[cfg(windows)]
-        {
-            self.check_disk_space_windows(&target_dir)?;
-        }
-
-        Ok(())
-    }
-
-    #[cfg(unix)]
-    fn check_disk_space_unix(&self, target_dir: &Path) -> Result<()> {
-        use std::ffi::CString;
-        use std::mem;
-
-        let c_path = CString::new(target_dir.to_string_lossy().as_bytes())?;
-        let mut stat: libc::statvfs = unsafe { mem::zeroed() };
-
-        let result = unsafe { libc::statvfs(c_path.as_ptr(), &mut stat) };
-
-        if result == 0 {
-            let available_mb = (stat.f_bavail * stat.f_frsize) / (1024 * 1024);
-            log::debug!(
-                "Disk space check: available={}MB, required={}MB",
-                available_mb,
-                self.min_disk_space_mb
-            );
-
-            if available_mb < self.min_disk_space_mb {
-                return Err(KopiError::DiskSpaceError(format!(
-                    "Insufficient disk space at {:?}. Required: {}MB, Available: {}MB",
-                    target_dir, self.min_disk_space_mb, available_mb
-                )));
-            }
-        } else {
-            let errno = std::io::Error::last_os_error();
-            log::error!("Failed to check disk space at {:?}: {}", target_dir, errno);
-            return Err(KopiError::SystemError(format!(
+        // Use fs2 for platform-independent disk space checking
+        let space_info = fs2::available_space(&target_dir).map_err(|e| {
+            log::error!("Failed to check disk space at {:?}: {}", target_dir, e);
+            KopiError::SystemError(format!(
                 "Failed to check disk space at {:?}: {}",
-                target_dir, errno
-            )));
-        }
+                target_dir, e
+            ))
+        })?;
 
-        Ok(())
-    }
+        let available_mb = space_info / (1024 * 1024);
+        log::debug!(
+            "Disk space check: available={}MB, required={}MB",
+            available_mb,
+            self.min_disk_space_mb
+        );
 
-    #[cfg(windows)]
-    fn check_disk_space_windows(&self, target_dir: &Path) -> Result<()> {
-        use std::os::windows::ffi::OsStrExt;
-        use std::ptr;
-        use winapi::um::errhandlingapi::GetLastError;
-        use winapi::um::fileapi::GetDiskFreeSpaceExW;
-
-        let path_wide: Vec<u16> = target_dir
-            .as_os_str()
-            .encode_wide()
-            .chain(Some(0))
-            .collect();
-
-        let mut available_bytes: u64 = 0;
-        let result = unsafe {
-            GetDiskFreeSpaceExW(
-                path_wide.as_ptr(),
-                &mut available_bytes as *mut u64,
-                ptr::null_mut(),
-                ptr::null_mut(),
-            )
-        };
-
-        if result != 0 {
-            let available_mb = available_bytes / (1024 * 1024);
-            log::debug!(
-                "Disk space check: available={}MB, required={}MB",
-                available_mb,
-                self.min_disk_space_mb
-            );
-
-            if available_mb < self.min_disk_space_mb {
-                return Err(KopiError::DiskSpaceError(format!(
-                    "Insufficient disk space at {:?}. Required: {}MB, Available: {}MB",
-                    target_dir, self.min_disk_space_mb, available_mb
-                )));
-            }
-        } else {
-            let error_code = unsafe { GetLastError() };
-            log::error!(
-                "Failed to check disk space at {:?}: Windows error code {}",
-                target_dir,
-                error_code
-            );
-            return Err(KopiError::SystemError(format!(
-                "Failed to check disk space at {:?}: Windows error code {}",
-                target_dir, error_code
+        if available_mb < self.min_disk_space_mb {
+            return Err(KopiError::DiskSpaceError(format!(
+                "Insufficient disk space at {:?}. Required: {}MB, Available: {}MB",
+                target_dir, self.min_disk_space_mb, available_mb
             )));
         }
 
